667d24a22ef4cba8fb253af601c815989d196aae
FbGod <mvb24f@gmail.com>
2022-07-16 16:43:13 +0300

    switch to full mapkit, organisations search

--- app/src/main/java/ru/examp/restaurantsnearby/MainActivity.java
+++ app/src/main/java/ru/examp/restaurantsnearby/MainActivity.java
@@ -1,125 +1,235 @@
 package ru.examp.restaurantsnearby;
 
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.GeoObjectCollection;
+import com.yandex.mapkit.map.CameraListener;
+import com.yandex.mapkit.map.CameraUpdateReason;
+import com.yandex.mapkit.map.Map;
+import com.yandex.mapkit.map.MapObjectCollection;
+import com.yandex.mapkit.search.BusinessObjectMetadata;
+import com.yandex.mapkit.search.Response;
+
 import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.layers.ObjectEvent;
-import com.yandex.mapkit.location.FilteringMode;
-import com.yandex.mapkit.location.Location;
-import com.yandex.mapkit.location.LocationListener;
-import com.yandex.mapkit.location.LocationManager;
-import com.yandex.mapkit.location.LocationStatus;
 import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CompositeIcon;
+import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.map.RotationType;
+import com.yandex.mapkit.map.VisibleRegionUtils;
 import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.search.SearchFactory;
+import com.yandex.mapkit.search.SearchManager;
+import com.yandex.mapkit.search.SearchManagerType;
+import com.yandex.mapkit.search.SearchOptions;
+import com.yandex.mapkit.search.Session;
 import com.yandex.mapkit.user_location.UserLocationLayer;
 import com.yandex.mapkit.user_location.UserLocationObjectListener;
 import com.yandex.mapkit.user_location.UserLocationView;
+import com.yandex.runtime.Error;
+import com.yandex.runtime.image.ImageProvider;
+import com.yandex.runtime.network.NetworkError;
+import com.yandex.runtime.network.RemoteError;
 
-import org.jetbrains.annotations.NotNull;
+/**
+ * This example shows how to display and customize user location arrow on the map.
+ */
+public class MainActivity extends Activity implements UserLocationObjectListener, Session.SearchListener, CameraListener {
+    /**
+     * Replace "your_api_key" with a valid developer key.
+     * You can get it at the https://developer.tech.yandex.ru/ website.
+     */
+    private final String MAPKIT_API_KEY = "6a673eca-16bb-4c2a-98b9-acc88b012a1f";
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 1;
 
-public class MainActivity extends AppCompatActivity implements UserLocationObjectListener {
-
-    private MapView mapview;
-
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private static final double DESIRED_ACCURACY = 0;
-    private static final long MINIMAL_TIME = 1000;
-    private static final double MINIMAL_DISTANCE = 1;
-    private static final boolean USE_IN_BACKGROUND = false;
     private MapView mapView;
-    private LocationManager locationManager;
-    private LocationListener myLocationListener;
-    private Point myLocation;
-    private boolean isLocationFound = false;
+    private UserLocationLayer userLocationLayer;
+    private EditText searchEdit;
+    private SearchManager searchManager;
+    private Session searchSession;
 
-    // убираем привязку камеры к местоположению устройства, лишь единажды наведя камеру
-    public void showUserPositionOnceOnMap() {
-        if (!isLocationFound) {
-            isLocationFound = true;
-            mapview.getMap().move(
-                    new CameraPosition(new Point(myLocation.getLatitude(), myLocation.getLongitude()), 17.0f, 0.0f, 0.0f),
-                    new Animation(Animation.Type.SMOOTH, 0),
-                    null);
+    private void submitQuery(String query) {
+        searchSession = searchManager.submit(
+                query,
+                VisibleRegionUtils.toPolygon(mapView.getMap().getVisibleRegion()),
+                new SearchOptions(),
+                this);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        MapKitFactory.initialize(this);
+        SearchFactory.initialize(this);
+        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        mapView = findViewById(R.id.mapview);
+        mapView.getMap().setRotateGesturesEnabled(false);
+        mapView.getMap().move(new CameraPosition(new Point(0, 0), 14, 0, 0));
+
+        requestLocationPermission();
+
+        MapKit mapKit = MapKitFactory.getInstance();
+        mapKit.resetLocationManagerToDefault();
+        userLocationLayer = mapKit.createUserLocationLayer(mapView.getMapWindow());
+        userLocationLayer.setVisible(true);
+        userLocationLayer.setHeadingEnabled(true);
+
+        userLocationLayer.setObjectListener(this);
+
+        searchManager = SearchFactory.getInstance().createSearchManager(SearchManagerType.COMBINED);
+
+        searchEdit = (EditText)findViewById(R.id.search_edit);
+        searchEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    submitQuery(searchEdit.getText().toString());
+                }
+
+                return false;
+            }
+        });
+
+//        mapView.getMap().move(
+//                new CameraPosition(new Point(59.945933, 30.320045), 14.0f, 0.0f, 0.0f)); обновление данных с движением камеры
+
+        submitQuery(searchEdit.getText().toString());
+
+
+
+    }
+
+    @Override
+    public void onSearchResponse(@NonNull Response response) {
+        MapObjectCollection mapObjects = mapView.getMap().getMapObjects();
+        mapObjects.clear();
+
+        for (GeoObjectCollection.Item searchResult : response.getCollection().getChildren()) {
+
+
+            System.out.println(searchResult.getObj().getName());
+            System.out.println(searchResult.getObj().getDescriptionText());
+            System.out.println(searchResult.getObj().getMetadataContainer().getItem(BusinessObjectMetadata.class).getPhones().get(0).getNumber());
+            System.out.println(searchResult.getObj().getMetadataContainer().getItem(BusinessObjectMetadata.class).getPhones().get(0).getFormattedNumber());
+            System.out.println(searchResult.getObj().getMetadataContainer().getItem(BusinessObjectMetadata.class).getWorkingHours().getText());
+
+
+            Point resultLocation = searchResult.getObj().getGeometry().get(0).getPoint();
+            if (resultLocation != null) {
+                mapObjects.addPlacemark(
+                        resultLocation,
+                        ImageProvider.fromResource(this, R.drawable.search_result));
+            }
         }
     }
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        String MAPKIT_API_KEY = "6a673eca-16bb-4c2a-98b9-acc88b012a1f";
-        MapKitFactory.setApiKey(MAPKIT_API_KEY);
-        MapKitFactory.initialize(this);
-        setContentView(R.layout.activity_main);
-        mapview = findViewById(R.id.mapview);
+    public void onSearchError(Error error) {
+//        String errorMessage = getString(R.string.unknown_error_message);
+//        if (error instanceof RemoteError) {
+//            errorMessage = getString(R.string.remote_error_message);
+//        } else if (error instanceof NetworkError) {
+//            errorMessage = getString(R.string.network_error_message);
+//        }
+//
+//        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+    }
 
-        // show user's location
-        MapKit mapKit = MapKitFactory.getInstance();
-        UserLocationLayer userLocationLayer = mapKit.createUserLocationLayer(mapview.getMapWindow());
-        userLocationLayer.setVisible(true); // Sets user location visibility
-        userLocationLayer.setHeadingEnabled(true);
-        userLocationLayer.setObjectListener(this);
 
-        // get user's latitude and longitude
-        mapView = (MapView) mapview;
-        locationManager = MapKitFactory.getInstance().createLocationManager();
-        myLocationListener = new LocationListener() {
-            @Override
-            public void onLocationUpdated(@NonNull @NotNull Location location) {
-                myLocation = location.getPosition(); //this user point
-                Log.w(TAG, "my location - " + myLocation.getLatitude() + "," + myLocation.getLongitude());
-                showUserPositionOnceOnMap();
-            }
 
-            @Override
-            public void onLocationStatusUpdated(@NonNull @NotNull LocationStatus locationStatus) {
-            }
-        };
+
+    private void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                "android.permission.ACCESS_FINE_LOCATION")
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{"android.permission.ACCESS_FINE_LOCATION"},
+                    PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
     }
 
     @Override
     protected void onStop() {
-        super.onStop();
-        mapview.onStop();
-        MapKitFactory.getInstance().onStop();
-
-        locationManager.unsubscribe(myLocationListener);
         mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        mapview.onStart();
         MapKitFactory.getInstance().onStart();
-
         mapView.onStart();
-        subscribeToLocationUpdate();
     }
 
     @Override
-    public void onObjectAdded(@NonNull @org.jetbrains.annotations.NotNull UserLocationView userLocationView) {
+    public void onObjectAdded(UserLocationView userLocationView) {
+        userLocationLayer.setAnchor(
+                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.5)),
+                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.83)));
+
+        userLocationView.getArrow().setIcon(ImageProvider.fromResource(
+                this, R.drawable.user_arrow));
+
+        CompositeIcon pinIcon = userLocationView.getPin().useCompositeIcon();
+
+//        pinIcon.setIcon(
+//                "icon",
+//                ImageProvider.fromResource(this, R.drawable.icon),
+//                new IconStyle().setAnchor(new PointF(0f, 0f))
+//                        .setRotationType(RotationType.ROTATE)
+//                        .setZIndex(0f)
+//                        .setScale(1f)
+//        );
+
+        pinIcon.setIcon(
+                "pin",
+                ImageProvider.fromResource(this, R.drawable.search_result),
+                new IconStyle().setAnchor(new PointF(0.5f, 0.5f))
+                        .setRotationType(RotationType.ROTATE)
+                        .setZIndex(1f)
+                        .setScale(0.5f)
+        );
+
+        userLocationView.getAccuracyCircle().setFillColor(Color.BLUE & 0x99ffffff);
+    }
+
+
+
+
+    @Override
+    public void onObjectRemoved(UserLocationView view) {
     }
 
     @Override
-    public void onObjectRemoved(@NonNull @org.jetbrains.annotations.NotNull UserLocationView userLocationView) {
+    public void onObjectUpdated(UserLocationView view, ObjectEvent event) {
     }
 
     @Override
-    public void onObjectUpdated(@NonNull @org.jetbrains.annotations.NotNull UserLocationView userLocationView, @NonNull @org.jetbrains.annotations.NotNull ObjectEvent objectEvent) {
-    }
-
-    // https://yandex.ru/dev/maps/mapkit/doc/android-ref/lite/com/yandex/mapkit/location/LocationManager.html#_4
-    private void subscribeToLocationUpdate() {
-        if (locationManager != null && myLocationListener != null) {
-            locationManager.subscribeForLocationUpdates(DESIRED_ACCURACY, MINIMAL_TIME, MINIMAL_DISTANCE, USE_IN_BACKGROUND, FilteringMode.OFF, myLocationListener);
+    public void onCameraPositionChanged(
+            Map map,
+            CameraPosition cameraPosition,
+            CameraUpdateReason cameraUpdateReason,
+            boolean finished) {
+        if (finished) {
+            submitQuery(searchEdit.getText().toString());
         }
     }
 }
